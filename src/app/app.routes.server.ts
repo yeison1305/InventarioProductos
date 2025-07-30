10552d4caf05b7b87099354aa7caab9b94c5989d
yeisonduque1305 <yeisonduque1305@gmail.com>
2025-07-29 22:11:11 -0500

    otra prueba

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Server, // Cambia a Server para evitar errores de prerendering
+  },
+  {
+    path: 'add',
+    renderMode: RenderMode.Server, // Cambia a Server para evitar errores de prerendering
+  },
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Server, // Mantiene Server como en la solución previa
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
